6e0405e3e98c4919915c6487e8c04e5f0239bc6e
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-07-08 15:53:12 +0600

    login controller

---
+++ controllers/loginController.js
@@ -0,0 +1,33 @@
+const { jwtToken } = require("../helpers/token");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const loginController = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "The email address you entered is not connected to an account",
+      });
+    }
+    bcrypt.compare(password, user.password).then(function (result) {
+      if (!result) {
+        return res.status(400).json({ message: "Incorrect your password" });
+      }
+      const token = jwtToken(user._id, "7d");
+      res.status(200).json({
+        id: user._id,
+        username: user.username,
+        profilePicture: user.profilePicture,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        token: token,
+        verified: user.verified,
+        message: "Login successful ",
+      });
+    });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+module.exports = loginController;
--- router/api/auth.js
+++ router/api/auth.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const registrationController = require("../../controllers/registrationController");
+const loginController = require("../../controllers/loginController");
 const verifiedUserController = require("../../controllers/verifiedUser");
 
 //TODO: method change this get == post
 router.post("/", registrationController);
 router.post("/activate", verifiedUserController);
+router.post("/login", loginController);
 
 module.exports = router;
