bfa6978e0c5d8a1ab657ff68f9c4efe5325d40ca
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-09-04 14:09:01 +0600

    post model created

--- controllers/changePassword.js
+++ controllers/changePassword.js
@@ -4,7 +4,14 @@ const changePassword = async (req, res, next) => {
   try {
     const { password, email } = req.body;
     const newPass = bcrypt.hashSync(password, 12);
-    await User.findOneAndUpdate({ email: email }, { password: newPass });
+    const updateResult = await User.findOneAndUpdate(
+      { email: email },
+      { password: newPass },
+      { new: true }
+    );
+    if (!updateResult) {
+      return res.status(404).json({ message: "user not found" });
+    }
     return res.status(200).json({ message: "Password reset successfully" });
   } catch (err) {
     return res.status(400).json({ message: err.message });
---
+++ models/Post.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const postModel = new Schema(
+  {
+    type: {
+      type: String,
+      enum: ["profilePicture", "cover", null],
+      default: null,
+    },
+    images: {
+      type: Array,
+    },
+    text: {
+      type: String,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    comments: [
+      {
+        text: {
+          type: String,
+        },
+        image: {
+          type: String,
+        },
+        commentedBy: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+        commentedAt: {
+          type: Date,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = model("PostModel", postModel);
