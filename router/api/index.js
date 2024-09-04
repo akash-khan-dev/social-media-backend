12c17ef18e173ac30b5ccc0522009bf5ed0cb756
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-09-04 14:12:59 +0600

    post route creted

--- router/api/index.js
+++ router/api/index.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const authRoute = require("./auth");
-
+const postRoute = require("./post");
 // const api = process.env.API;
 
 router.use("/auth", authRoute);
+router.use("/post", postRoute);
 module.exports = router;
---
+++ router/api/post.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+const verifiedUserController = require("../../controllers/verifiedUser");
+
+// router.post("/createPost", registrationController);
+
+module.exports = router;
