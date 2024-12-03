22ff670ff27017cf5b56324968c18a74cad580fc
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-12-03 23:59:39 +0600

    user info populate for post show

--- controllers/showPost.js
+++ controllers/showPost.js
@@ -1,7 +1,9 @@
 const PostModel = require("../models/Post");
 const showPosts = async (req, res) => {
   try {
-    const posts = await PostModel.find();
+    const posts = await PostModel.find()
+      .populate("user", "firstName lastName username profilePicture")
+      .sort({ createdAt: -1 });
     return res.status(200).json({ data: posts });
   } catch (err) {
     return res.status(404).json({ message: err.message });
