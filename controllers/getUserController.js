5b836806f82c9d76af28d6ab0089fe9f46920629
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-12-07 21:50:56 +0600

    profile info with post get

--- controllers/getUserController.js
+++ controllers/getUserController.js
@@ -1,12 +1,18 @@
 const User = require("../models/userModel");
+const PostModel = require("../models/Post");
 
 const getUserController = async (req, res, next) => {
   const { username } = req.params;
   try {
     const user = await User.findOne({ username: username }).select("-password");
-    return res.status(200).json({ user: user });
+    if (!user) {
+      return res.json({ message: "User not found", ok: false });
+    }
+
+    const post = await PostModel.find({ user: user._id }).populate("user");
+    return res.status(200).json({ ...user.toObject(), post });
   } catch (e) {
-    return res.status(404).json({ message: e.message });
+    return res.status(500).json({ message: e.message, ok: false });
   }
 };
 module.exports = getUserController;
