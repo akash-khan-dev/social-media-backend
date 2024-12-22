851e19b21a09d35da499318dca8b78cf95cfbcf5
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-12-22 23:00:44 +0600

    proper getuser data

--- controllers/getUserController.js
+++ controllers/getUserController.js
@@ -3,16 +3,41 @@ const PostModel = require("../models/Post");
 
 const getUserController = async (req, res, next) => {
   const { username } = req.params;
+  const { userId } = req.body;
   try {
-    const user = await User.findOne({ username: username }).select("-password");
-    if (!user) {
+    const currentUser = await User.findById(userId);
+    const getProfile = await User.findOne({ username: username }).select(
+      "-password"
+    );
+    const friendShip = {
+      friend: false,
+      following: false,
+      request: false,
+      requestReceived: false,
+    };
+    if (!getProfile) {
       return res.json({ message: "User not found", ok: false });
     }
 
-    const post = await PostModel.find({ user: user._id })
+    if (
+      currentUser.friends.includes(getProfile._id) &&
+      getProfile.includes(currentUser._id)
+    ) {
+      friendShip.friend = true;
+    }
+    if (currentUser.following.includes(getProfile._id)) {
+      friendShip.following = true;
+    }
+    if (getProfile.request.includes(currentUser._id)) {
+      friendShip.request = true;
+    }
+    if (currentUser.request.includes(getProfile._id)) {
+      friendShip.requestReceived = true;
+    }
+    const post = await PostModel.find({ user: getProfile._id })
       .populate("user")
       .sort({ createdAt: -1 });
-    return res.status(200).json({ ...user.toObject(), post });
+    return res.status(200).json({ ...user.toObject(), post, friendShip });
   } catch (e) {
     return res.status(500).json({ message: e.message, ok: false });
   }
